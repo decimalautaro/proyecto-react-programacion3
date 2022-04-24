c9793fabe73cf650362c6b31d37ac199edcf8304
Lautaro Decima <decimalautaro@gmail.com>
2022-04-24 20:13:30 -0300

    creacion de post tareas

--- src/routes/tareasRoutes.ts
+++ src/routes/tareasRoutes.ts
@@ -14,7 +14,32 @@ router.get('/',(req: Request, res: Response )=>{
     res.send(tareas);
 })
 
+router.post('/',(req: Request, res: Response)=>{
+    const nuevaTarea = req.body as Tarea;
+    if (!nuevaTarea?. nombre){
+        res.status(400).send({
+            message: 'el archivo no existe';
 
+        })
+    }
+
+    //creacion de id
+    nuevaTarea.id = tareas[tareas.length-1].id+1;
+
+    //agregar al array tareas
+    tareas.push(nuevaTarea);
+
+    //devolvemos el generado
+    res.status(201).send(nuevaTarea);
+
+})
+
+
+//PUT
+
+//GET by ID
+
+//DELETE
 
 
 
