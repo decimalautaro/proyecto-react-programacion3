f869a50f97165b78fbc5037a0705048e23178002
Lautaro Decima <decimalautaro@gmail.com>
2022-05-05 14:27:58 -0300

    conexion con la base de datos

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import express from 'express';
-import tareasRoutes from './routes/tareasRoutes';
+import { connect } from 'mongoose';
+import { Tarea } from './models/Tarea';
+import tareasRouter from './routes/tareasRoutes'
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5005;
+const DB_NAME = 'prog3-2022';
+const DB_CONN = `mongodb://localhost:27017/${DB_NAME}`;
 const server = express();
 
 server.use(express.json());
+server.use("/api/tareas", tareasRouter);
 
 
-server.use('/api/tareas',tareasRoutes);
 
+const run = async () => {
+    await connect(DB_CONN);
+    console.log("Se ha conectado a la base de datos");
 
-server.listen(PORT, ()=>{
-    console.log(`Servidor escuchando en puerto ${PORT}`);
-})
+    server.listen(PORT, () => {
+        console.info(`Iniciando servidor en puerto ${PORT}`)
+    });
+
+};
+
+run().catch(err => console.log(err));
